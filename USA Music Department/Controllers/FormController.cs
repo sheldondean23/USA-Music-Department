15b97d2c62ac8b3592348c11e8fbc32c823384e8
DREAM <sheldon.dean.23@gmail.com>
2016-01-26 00:09:46 -0600

    DB entity moved. and student form started

---
+++ USA Music Department/Controllers/FormController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace USA_Music_Department.Controllers
+{
+    public class FormController : Controller
+    {
+        //private 
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Form/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Form/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Form/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Form/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Form/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Form/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Form/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
