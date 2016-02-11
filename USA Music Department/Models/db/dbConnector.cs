c6699126a31fa4d6a1c13bb533eb2ccb608ef75a
DREAM <sheldon.dean.23@gmail.com>
2016-02-11 14:56:13 -0600

    database connection class and empty student manipulation class added

---
+++ USA Music Department/Models/db/dbConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace USA_Music_Department.Models.db
+{
+    public class dbConnector
+    {
+        public SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["BandStudentDBEntities"].ConnectionString);
+        public int rowsAffected;
+    }
+}
