d7cd58ce06aac06ae1b06886fedc7289f0f448f7
Betofall <gm2012@bellsouth.net>
2016-02-16 13:14:10 -0600

    404 Error Page

---
+++ USA Music Department/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace USA_Music_Department.Controllers
+{ 
+    public class ErrorController : Controller
+
+    {
+        public ViewResult Index()
+        {
+            return View("Error");
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+
+}
